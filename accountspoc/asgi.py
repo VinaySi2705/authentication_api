963a38d6433cf07e549cf4ccbe9797911d75c482
Vinay Singh Yadav <vinaysinghyadav9125@gmail.com>
2021-11-29 16:11:12 +0530

    first commit

---
+++ accountspoc/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for accountspoc project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'accountspoc.settings')
+
+application = get_asgi_application()
