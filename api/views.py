63d4eb150d37aa599f3b89167eee766b11a2fb6d
Vinay Singh Yadav <vinaysinghyadav9125@gmail.com>
2021-12-01 12:17:21 +0530

    last commit

--- api/views.py
+++ api/views.py
@@ -9,6 +9,30 @@ from rest_framework import permissions
 from django.contrib.auth.hashers import make_password
 from rest_framework.authentication import BasicAuthentication
 
+# reset password email
+from django.dispatch import receiver
+from django.urls import reverse
+from django_rest_passwordreset.signals import reset_password_token_created
+from django.core.mail import send_mail
+
+
+@receiver(reset_password_token_created)
+def password_reset_token_created(sender, instance, reset_password_token, *args, **kwargs):
+
+    email_plaintext_message = "{}?token={}".format(reverse('password_reset:reset-password-request'), reset_password_token.key)
+
+    send_mail(
+        # title:
+        "go to http://127.0.0.1:8000/password_reset/confirm/ and there post token and new password",
+        # message:
+        email_plaintext_message,
+        # from:
+        "noreply@somehost.local",
+        # to:
+        [reset_password_token.user.email]
+    )
+
+
 #signup of users
 class RegisterView(generics.CreateAPIView):
     serializer_class = UserSerializer
